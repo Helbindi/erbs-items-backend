579bc54376c8185abfa7f0131b578030bf8d8641
Andy <nunuzuhh@gmail.com>
2024-08-02 15:32:38 -0700

    cors moved to specific routes for security reasons.

--- routes/items.js
+++ routes/items.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import {
   getAllItems,
   getItem,
@@ -6,21 +7,33 @@ import {
   updateItem,
   deleteItem,
 } from "../controllers/itemControllers.js";
-const router = express.Router();
 
+const corsPublic = {
+  methods: ["GET"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+const corsDev = {
+  origin: ["http://localhost:8080/", "http://localhost:5173/"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+const router = express.Router();
 // Get all items
-router.get("/", getAllItems);
+router.get("/", cors(corsPublic), getAllItems);
 
 // Get single item
-router.get("/:id", getItem);
+router.get("/:id", cors(corsPublic), getItem);
 
 // Create new item
-router.post("/", createItem);
+router.post("/", cors(corsDev), createItem);
 
 // Update a item
-router.put("/:id", updateItem);
+router.put("/:id", cors(corsDev), updateItem);
 
 // Delete a item
-router.delete("/:id", deleteItem);
+router.delete("/:id", cors(corsDev), deleteItem);
 
 export default router;
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import mongoose from "mongoose";
 import items from "./routes/items.js";
 import logger from "./middleware/logger.js";
@@ -8,21 +7,12 @@ import notFound from "./middleware/notFound.js";
 import "dotenv/config";
 
 const app = express();
-// const env = dotenv.config().parsed;
 const port = process.env.PORT || 8000;
 
 // Connect to MongoDB
 mongoose.connect(
   `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.qp0dovg.mongodb.net/EternalReturn?retryWrites=true&w=majority&appName=Cluster0`
 );
-
-// Cors
-const corsOptions = {
-  methods: ["GET", "POST", "PUT", "DELETE"], // Only allow certain requests
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-};
-app.use(cors(corsOptions));
 
 // Body parser middleware
 app.use(express.json());
